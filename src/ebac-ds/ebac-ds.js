33e68fe3d1b49842906db8fd135d83666fc003ab
Lucas Hoffmann <lucashoffmann99@gmail.com>
2024-01-30 20:20:14 -0300

    Removing unused files

--- src/ebac-ds/ebac-ds.js
+++ src/ebac-ds/ebac-ds.js
@@ -1,8 +1,7 @@
 import Card from "./card/Card";
 import Button from "./button/Button";
-import Text from "./text/Text";
 import Divider from "./divider/Divider";
 import Box from "./box/Box";
 import Container from "./container/Container";
 
-export { Card, Button, Text, Divider, Box, Container };
+export { Card, Button, Divider, Box, Container };
